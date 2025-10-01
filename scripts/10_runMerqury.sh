46d5f42808702b377b28c79996081d24dd9fde04
yjauslin <yannick.jauslin@students.unibe.ch>
2025-10-01 16:24:31 +0200

    modified 10_runMerqury.sh

--- scripts/10_runMerqury.sh
+++ scripts/10_runMerqury.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=00:30:00
-#SBATCH --mem=1G
-#SBATCH --cpus-per-task=1
+#SBATCH --time=01:00:00
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task=16
 #SBATCH --job-name=merqury
 #SBATCH --mail-user=yannick.jauslin@students.unibe.ch
 #SBATCH --mail-type=end
@@ -15,17 +15,17 @@
 WORK_DIR=/data/users/${USER}/assembly_annotation_course
 
 #save output directory as variable
-OUT_DIR=$WORK_DIR/mercury
+OUT_DIR="${WORK_DIR}/mercury"
 
 #save assembly directories as variables
-FLYE_DIR=$WORK_DIR/flye_assembly
+FLYE_DIR=${WORK_DIR}/flye_assembly
 
-HIFIASM_DIR=$WORK_DIR/hifiasm_assembly
+HIFIASM_DIR=${WORK_DIR}/hifiasm_assembly
 
-LJA_DIR=$WORK_DIR/LJA_assembly
+LJA_DIR=${WORK_DIR}/LJA_assembly
 
 #save container directory as variable
-CONTAINER=containers/apptainer/merqury_1.3.sif
+CONTAINER="containers/apptainer/merqury_1.3.sif"
 
 #save assemblies as variables
 FLYE=${FLYE_DIR}/assembly.fasta
@@ -35,7 +35,7 @@ HIFIASM=${HIFIASM_DIR}/ERR11437318.bp.p_ctg.fa
 LJA=${LJA_DIR}/assembly.fasta
 
 #create arrays for input directories and assemblies
-input_dir=($FLYE_DIR $HIFIASM_DIR $LJA_DIR)
+#input_dir=($FLYE_DIR $HIFIASM_DIR $LJA_DIR)
 assemblies=($FLYE $HIFIASM $LJA)
 
 #create label-array for nice output names later on
@@ -43,10 +43,7 @@ labels=(flye hifiasm lja)
 
 export MERQURY="/usr/local/share/merqury"
 
-#move to respective input directory
-cd ${input_dir[$SLURM_ARRAY_TASK_ID]}
-
 #evaluate each assembly with mercury
-apptainer exec --bind ${input_dir[$SLURM_ARRAY_TASK_ID]} $CONTAINER ${MERQURY}/merqury.sh \
+apptainer exec --bind $WORK_DIR $CONTAINER ${MERQURY}/merqury.sh \
  $OUT_DIR/reads.meryl ${assemblies[$SLURM_ARRAY_TASK_ID]} ${labels[$SLURM_ARRAY_TASK_ID]} \
  > flye_merqury.log 2>&1
