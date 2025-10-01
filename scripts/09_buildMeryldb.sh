da1df917d2a14e7742300e2cdaf361b4b76f97e5
yjauslin <yannick.jauslin@students.unibe.ch>
2025-10-01 16:34:32 +0200

    added comments to 09

--- scripts/09_buildMeryldb.sh
+++ scripts/09_buildMeryldb.sh
@@ -10,6 +10,7 @@
 #SBATCH --error=/data/users/yjauslin/error_meryl_%j.e
 #SBATCH --partition=pibu_el8
 
+#save working-, container-, reads- and output directories as variables
 WORK_DIR=/data/users/${USER}/assembly_annotation_course
 
 CONTAINER="/containers/apptainer/merqury_1.3.sif"
@@ -18,7 +19,9 @@ READS="${WORK_DIR}/Hiroshima/ERR11437318.fastq.gz"
 
 OUT_DIR=${WORK_DIR}/mercury
 
+#create output directory
 mkdir $OUT_DIR
 
+#create meryldb for the best kmer count obtained from the results of 08_getBestKmer.sh
 apptainer exec --bind /data ${CONTAINER} meryl k=19 count \
   ${READS} output ${OUT_DIR}/reads.meryl     
